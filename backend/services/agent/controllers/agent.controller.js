77c5126b1c84d81ac8988057080e52f96a819f83
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-07-26 18:33:00 +0530

    changes in agent memory

--- backend/services/agent/controllers/agent.controller.js
+++ backend/services/agent/controllers/agent.controller.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { graph } from "../graph/graph.js";
+import { addMessage } from "../config/memory.js";
 export const agent = async (req, res) => {
   try {
     const { prompt, conversationId } = req.body;
+    await addMessage(conversationId, "user", prompt);
     await axios.post(`${process.env.CHAT_SERVICE}/save-converse`, {
       conversationId,
       role: "user",
       conent: prompt,
     });
+
     const result = await graph.invoke({
       prompt,
       conversationId,
     });
     const response = result.aiResponse;
+    await addMessage(conversationId, "assistant", response);
+    await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
+      conversationId,
+      role: "assistant",
+      content: response,
+    });
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({ message: `agent error ${error}` });
---
+++ frontend/src/components/MessageBubble.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {copy,Check} from "lucide-react";
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {Fix} from "rea"
+function MessageBubble({ role, content, images })
+{
+    const isUser = role == "user";
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MessageBubble
